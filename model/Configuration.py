95c46a7004525639906df8cc14699b94662c0a12
Alexis Marion <alexis.marion.8@hotmail.fr>
2018-10-26 11:02:50 +0200

    define get_words

--- model/Configuration.py
+++ model/Configuration.py
@@ -7,12 +7,15 @@ class Configuration:
     ref_arbre = []
 
     def __init__(self, filename):
+        self.word_list = []
         self.buffer = self.get_words(filename)
         self.oracle = Oracle(filename)
         self.ref_arbre = self.oracle.predict()
 
     def get_words(self, filename):
-        pass
+        phrases =  ''.join([line for line in open(filename, 'r')]).split('.')
+        words = [p.split(' ') for p in phrases]
+        return words
 
     def get_couple(self):
         return self.pile[-1], self.buffer[0]
